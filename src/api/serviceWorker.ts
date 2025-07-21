adecdab1488638a3cbedcbf583580f2f7ed323a9
danniel <1037897418@qq.com>
2025-07-22 00:49:57 +0800

    feat: fetchMdV2Api 根据字幕文本整理生成md的接口

--- src/api/serviceWorker.ts
+++ src/api/serviceWorker.ts
@@ -9,3 +9,15 @@ export const fetchMdApi = (data: object = {}) => {
     })
     .then(response => response.json())
 }
+
+export const fetchMdV2Api = (data: object = {}) => {
+  return fetch("http://127.0.0.1:4523/m1/6804322-6517891-default/v2/generate", {
+    // fetch("https://yt2note-production.up.railway.app/v2/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+}
