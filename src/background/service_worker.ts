2f471d1af9a7dd27ef02d152d18f9a5004f49e8d
liminghu <1037897418@qq.com>
2025-07-21 18:06:48 +0800

    在服务工作者中添加消息发送功能，以便在字幕下载过程中向用户反馈状态，包括加载、错误和成功信息。

--- src/background/service_worker.ts
+++ src/background/service_worker.ts
@@ -1,3 +1,5 @@
+import { sendMsgByServiceWorker } from "../utils/service-worker-utils";
+
 chrome.runtime.onInstalled.addListener(() => { 
   // 创建右键菜单
   chrome.contextMenus.create({
@@ -15,6 +17,9 @@ chrome.contextMenus.onClicked.addListener((data: chrome.contextMenus.OnClickData
       const url = new URL(data.pageUrl || "")
       const videoId = url.searchParams.get("v")
       if(url.hostname === "www.youtube.com" && videoId) {
+        // 获取当前活动标签页并发送消息给content script
+        sendMsgByServiceWorker("subtitle_downloading", "loading", "正在提取字幕...")
+        
         fetch("http://127.0.0.1:4523/m1/6804322-6517891-default/generate", {
         // fetch("https://yt2note-production.up.railway.app/generate", {
           method: "POST",
@@ -32,6 +37,7 @@ chrome.contextMenus.onClicked.addListener((data: chrome.contextMenus.OnClickData
           // 验证数据完整性
           if (!data || !data.content) {
             console.error('API返回数据格式错误:', data)
+            sendMsgByServiceWorker("subtitle_downloading", "error", "API返回数据格式错误")
             return
           }
           // 下载为md
@@ -55,17 +61,17 @@ chrome.contextMenus.onClicked.addListener((data: chrome.contextMenus.OnClickData
           const reader = new FileReader()
           reader.onload = () => {
             const dataUrl = reader.result as string
-            console.log('dataUrl: ', dataUrl)
-            console.log('filename: ', filename)
             chrome.downloads.download({ 
               url: dataUrl, 
               filename: filename,
-              saveAs: true
+              // saveAs: true
             }, (downloadId) => {
               if (chrome.runtime.lastError) {
                 console.error('下载失败:', chrome.runtime.lastError)
+                sendMsgByServiceWorker("subtitle_downloading", "error", "下载失败，请稍后重试")
               } else {
                 console.log('下载成功，下载ID:', downloadId)
+                sendMsgByServiceWorker("subtitle_downloading", "success", `${filename} 下载成功`)
               }
             })
           }
@@ -74,7 +80,10 @@ chrome.contextMenus.onClicked.addListener((data: chrome.contextMenus.OnClickData
         .catch(error => {
           console.error("API请求失败:", error);
           // 可以在这里添加用户通知
+          sendMsgByServiceWorker("subtitle_downloading", "error", `${error || 'API请求失败，请稍后重试'}`)
         });
+      } else {
+        sendMsgByServiceWorker("subtitle_downloading", "warning", "当前仅支持提供字幕的youtube视频，请检查视频链接是否正确")
       }
     })
   }
