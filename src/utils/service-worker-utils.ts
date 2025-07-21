dc63cf31c09cffbc19177569d9a3b575c6bf3ad2
liminghu <1037897418@qq.com>
2025-07-21 18:06:12 +0800

    添加服务工作者工具函数，service_worker向content方送消息

---
+++ src/utils/service-worker-utils.ts
@@ -0,0 +1,11 @@
+export const sendMsgByServiceWorker = (type: string, data: string, msg: string) => {
+  chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
+    if (tabs[0]?.id) {
+      chrome.tabs.sendMessage(tabs[0].id, {
+        type,
+        data,
+        msg: `【youtube字幕提取助手】：${msg}`
+      });
+    }
+  });
+} 
